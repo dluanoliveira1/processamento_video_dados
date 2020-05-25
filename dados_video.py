fbebe5d65f99371b95e9c07de47f37d1b332e20d
David <dluan.oliveira1@gmail.com>
2020-05-25 20:31:03 -0300

    finalizado sudoku

--- dados_video.py
+++ dados_video.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-
+# Funcão que define se os dados estão juntos (grudados)
 def isValidaDouble(width, height):
     if width > height:
         if width > 70 and width < 110:
@@ -12,7 +12,7 @@ def isValidaDouble(width, height):
 
     return False
 
-
+# Função que define se o tamanho é correspondente a um dado
 def isValid(width, height):
     if width > 35 and width < 70:
         if height > width - 10 and height < width + 10:
@@ -20,39 +20,38 @@ def isValid(width, height):
 
     return False
 
-
+# Função que identifica se o contorno da forma se manteve o mesmo após o frame
 def equalContour(w, lW):
-
     if w >= lW - 3 and w <= lW + 3:
         return True
 
     return False
 
-
+# Nesse função, é cortada a imagem de acordo com o tamanho e posição do dado, para então analisar o número do dado
 def diceValue(x, y, w, h, img, imgFinal):
+
+    # Recorta a imagem
     crop_img1 = img[y:y+h, x:x+w]
     crop_img = cv2.cvtColor(crop_img1, cv2.COLOR_BGR2GRAY)
 
-    # _,thresh1 = cv2.threshold(crop_img, 150, 255, cv2.THRESH_BINARY_INV)
+    # Faz o trabalho pra identificar os Contours
     _, thresh1 = cv2.threshold(crop_img, 205, 255, cv2.THRESH_BINARY_INV)
     contours1, hierarchy1 = cv2.findContours(
         thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     countValue = 0
 
-    # cv2.imshow("Thresh", thresh1)
-    # cv2.imshow("cropped", crop_img)
-    # cv2.waitKey(0)
-
     for contour1 in contours1:
         (x2, y2, w2, h2) = cv2.boundingRect(contour1)
 
         if w2 > 5 and w2 < 10 and h2 > 5 and h2 < 10:
+            # Desenha o contorno na Imagem final, a partir dos contours da imagem cortada (posição, tamanho)
             cv2.rectangle(imgFinal, (x2 + x, y2 + y),
-                          (x2+w2+x, y2+h2+y), (0, 255, 0), 1)
+                          (x2+w2+x, y2+h2+y), (255, 0, 0), 1)
             countValue += 1
 
+    # Escreve o numero de pontos encontrado, na imagem final
     cv2.putText(imgFinal, str(countValue), ((x + np.int(w/2) - 12), (y + np.int(h/2) + 5)),
-                cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1.5, (255, 0, 0), 1, cv2.LINE_AA)
+                cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 1.5, (0, 200, 0), 1, cv2.LINE_AA)
 
 
 # Inicializando auxiliares
@@ -100,8 +99,7 @@ while True:
             countWriteC = 0
             countWriteD = 0
 
-            # cv2.rectangle(frameFinal, (x, y), (x+w, y+h), (0, 0, 255), 1)
-
+            # A lógica dentro desse if é para identificar se os frames estão se repetindo, uma vez que se repetem x vezes, podemos mostrar o resultado
             if countContors == 1:
                 if equalContour(w, lastW) and equalContour(x, lastX) and equalContour(h, lastH) and equalContour(y, lastY):
                     countWriteA += 1
@@ -129,10 +127,6 @@ while True:
             countWriteB = 0
             if h > w:
                 refA = (int)(y + (h/2))
-                # cv2.rectangle(frameFinal, (x, y), (x+w, refA), (0, 0, 255), 1)
-                # cv2.rectangle(frameFinal, (x, refA),
-                #               (x+w, y+h), (0, 0, 255), 1)
-
                 if equalContour(int(h/2), lastH) and equalContour(x, lastX) and equalContour(w, lastW) and equalContour(y, lastY):
                     countWriteC += 1
                 else:
@@ -148,10 +142,6 @@ while True:
                 lastY1 = refA
             else:
                 refA = (int)(x + (w/2))
-                # cv2.rectangle(frameFinal, (x, y), (refA, y+h), (0, 0, 255), 1)
-                # cv2.rectangle(frameFinal, (refA, y),
-                #               (x+w, y+h), (0, 0, 255), 1)
-
                 if equalContour((int)(w/2), lastW) and equalContour(x, lastX) and equalContour(h, lastH) and equalContour(y, lastY):
                     countWriteD += 1
                 else:
@@ -166,24 +156,30 @@ while True:
                 lastX1 = refA
                 lastY1 = y
 
+    # Limpando variaveis
     if countContors == 0:
         countWriteA = 0
         countWriteB = 0
-
+    # Limpando variaveis
     if countContorsA == 0:
         countWriteC = 0
         countWriteD = 0
-
+    
+    # Logica para um dos dados (após 15 frames, chamaremos a função para identificar do dado (diceValue))
     if countWriteA > 15:
         diceValue(lastX, lastY, lastW, lastH, frame, frameFinal)
         cv2.rectangle(frameFinal, (lastX, lastY),
                       (lastX+lastW, lastY+lastH), (0, 0, 255), 1)
 
+    # Logica para o dado remanescente (após 15 frames passando pela validação que se manteve +-3 comparando com o anterior, 
+    # chamaremos a função para identificar do dado (diceValue))
     if countWriteB > 15:
         diceValue(lastX1, lastY1, lastW1, lastH1, frame, frameFinal)
         cv2.rectangle(frameFinal, (lastX1, lastY1),
                       (lastX1+lastW1, lastY1+lastH1), (0, 0, 255), 1)
 
+    # Logica para os dados juntos (chama diceVlaue após 18 frames que passam pela doubleValid, 
+    # que identifica se o frame manteve-se o mesmo, com uma carencia +-3)
     if countWriteC > 18:
         diceValue(lastX, lastY, lastW, lastH, frame, frameFinal)
         diceValue(lastX1, lastY1, lastW1, lastH1, frame, frameFinal)
@@ -192,6 +188,7 @@ while True:
         cv2.rectangle(frameFinal, (lastX1, lastY1),
                       (lastX1+lastW1, lastY1+lastH1), (0, 0, 255), 1)
 
+    # Mesma coisa que o anterior, porem para os dados na outra posição (um vertical e outro horizontal)
     if countWriteD > 18:
         diceValue(lastX, lastY, lastW, lastH, frame, frameFinal)
         diceValue(lastX1, lastY1, lastW1, lastH1, frame, frameFinal)
@@ -200,6 +197,7 @@ while True:
         cv2.rectangle(frameFinal, (lastX1, lastY1),
                       (lastX1+lastW1, lastY1+lastH1), (0, 0, 255), 1)
 
+    # Counting ++ e limpando variaveis
     countFrame += 1
     countContors = 0
     countContorsA = 0
@@ -207,8 +205,8 @@ while True:
     print('\n')
     print(countFrame)
 
-    cv2.imshow("Frame", frameFinal)
-    cv2.imshow("Mask", thresh)
+    cv2.imshow("FrameByFrame", frameFinal)
+    # cv2.imshow("Mask", thresh)
 
     key = cv2.waitKey(1)
     if key == 113:
--- sudoku.py
+++ sudoku.py
@@ -10,99 +10,74 @@ def isValid(width, height):
     return False
 
 
-imgUrl = "dados\\sudoku\\sudoku8.jpg"
+countingImg = 1
+while True:
 
-imgRef = cv2.imread(imgUrl)
-imgFinal = cv2.resize(imgRef, (700,700))
+    
+    
 
-img = cv2.imread(imgUrl, cv2.IMREAD_GRAYSCALE)
-image = cv2.resize(img, (700,700))
+    # Imagem que será processada
+    imgUrl = "dados\\sudoku\\sudoku" + str(countingImg) + ".jpg"
 
-_, thresh = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY_INV)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Imagem para escrever os resultados finais
+    imgRef = cv2.imread(imgUrl)
+    imgFinal = cv2.resize(imgRef, (700,700))
 
-for contour in contours:
-    (x, y, w, h) = cv2.boundingRect(contour)
-    # print(x,y,w,h)
+    # Imagem para utilizar durante processamento dos quadrados
+    img = cv2.imread(imgUrl, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(img, (700,700))
 
-    if w > 580 and h > 580 :
-        crop_img = image[y:y+h, x:x+w]
+    # Encontrando os boards dentro da imagem
+    _, thresh = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        _, thresh1 = cv2.threshold(crop_img, 240, 255, cv2.THRESH_BINARY)
-        contours1, hierarchy1 = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        print(x,y,w,h)
 
-        for contour1 in contours1:
-            (x1, y1, w1, h1) = cv2.boundingRect(contour1)
-            # print(x1,y1,w1,h1)
+        if w > 300 and h > 300 :
+            # Cortando os boards existentes para analisar de forma independente
+            crop_img_n = image[y:y+h, x:x+w]
+            # Aplicando blur para tirar os ruidos que interferem na obtenção dos quadrados
+            crop_img = cv2.GaussianBlur(crop_img_n, (5,3), cv2.BORDER_DEFAULT)
 
-            if isValid(h1, w1) :
-                crop_img1 = image[y+y1:y+y1+h1, x+x1:x+x1+w1]
+            # Encontrando os quadrados dentro do board
+            _, thresh1 = cv2.threshold(crop_img, 240, 255, cv2.THRESH_BINARY)
+            contours1, hierarchy1 = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                _, thresh2 = cv2.threshold(crop_img1, 125, 255, cv2.THRESH_BINARY_INV)
-                contours2, hierarchy2 = cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # Todos os quadrados do board (contornos)
+            for contour1 in contours1:
+                (x1, y1, w1, h1) = cv2.boundingRect(contour1)
 
-                for contour2 in contours2:
-                    (x2, y2, w2, h2) = cv2.boundingRect(contour2)
-                    print(x2,y2,w2,h2)
+                # Validando se o quadrado se encaixa no esperado
+                if isValid(h1, w1) :
+                    crop_img1 = image[y+y1:y+y1+h1, x+x1:x+x1+w1]
 
-                    if w > 30 and h > 30 :
+                    # Processando a image para obter os contornos que existem dentro de cada quadrado do board
+                    _, thresh2 = cv2.threshold(crop_img1, 115, 255, cv2.THRESH_BINARY_INV)
+                    contours2, hierarchy2 = cv2.findContours(thresh2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                    isNumber = False
+                    for contour2 in contours2:
+                        (x2, y2, w2, h2) = cv2.boundingRect(contour2)
+                        # print(x2,y2,w2,h2)
+
+                        if w2 > 1 and h2 >= 1 : 
+                            isNumber = True
+                    
+                    # Se existe algum sinal de cor dentro do quadrado do board, entende se que temos um numero ali
+                    if isNumber :
                         cv2.rectangle(imgFinal, (x+x1+5, y+y1+5),(x+x1+w1-5, y+y1+h1-5), (0, 0, 255), 2)
 
 
+   
+    countingImg += 1
+    countingImg = countingImg % 9
+    if countingImg == 0 : 
+        countingImg = 1
 
-
-
-
-cv2.imshow("img", imgFinal)
-# # cv2.imshow("crop", crop_img)
-# # cv2.imshow("thresh", thresh)
-# # cv2.imshow("thresh1", thresh1)
-cv2.waitKey()
-
-# x=14
-# y=14
-# h=772
-# w=772
-# crop_img1 = img[y:y+h, x:x+w]
-# crop_img = cv2.cvtColor(crop_img1, cv2.COLOR_BGR2GRAY)
-
-# # laplace = cv2.Laplacian(img,cv2.CV_8U)
-
-# # sobelX = cv2.Sobel(img,cv2.CV_8U,1,0,ksize=5)
-# # sobelY = cv2.Sobel(img,cv2.CV_8U,0,1,ksize=5)
-
-# # # Output dtype = cv2.CV_64F. Then take its absolute and convert to cv2.CV_8U
-# # sobelx64f = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
-# # abs_sobel64f = np.absolute(sobelx64f)
-# # sobel_64 = np.uint8(abs_sobel64f)
-
-# # Definindo área dos dados - threshDados
-# _, thresh = cv2.threshold(crop_img, 240, 255, cv2.THRESH_BINARY)
-
-# # Pegando os contornos dos dados das áreas dos dados
-# contours, hierarchy = cv2.findContours(
-#     thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# # Acessando todas as formas encontradas na imagem
-# for contour in contours:
-#     (x, y, w, h) = cv2.boundingRect(contour)
-
-#     print(x,y,w,h)
-#     cv2.rectangle(img, (x, y),(x+w, y+h), (0, 0, 255), 1)
-
-
-# cv2.imshow("img", img)
-# cv2.imshow("thresh", thresh)
-# cv2.waitKey()
-
-# # cv2.imshow('Original', img)
-# # cv2.imshow('Laplace', laplace)
-# # cv2.imshow('SobelX',  sobelX)
-# # cv2.imshow('SobelY', sobelY)
-# # cv2.imshow('Sobel64', sobel_64)
-
-# # cv2.waitKey()
-
-# # cv2.destroyAllWindows()
-
+    cv2.imshow("img", imgFinal)
+    key = cv2.waitKey(1000)
+    if key == 113:
+        break
 
